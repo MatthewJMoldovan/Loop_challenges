7207418d4b7ef3587286d3665797e35130d77f1f
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-01-27 20:30:55 -0700

    loop_challenges

---
+++ loop_challenges.js
@@ -0,0 +1,38 @@
+// Print odds 1-20
+// for(x=0; x<=20; x++){
+//     if(x%2!=0){
+//         console.log(x);
+//     }
+// }
+
+// Decreasing Multiples of 3
+// for(x=100; x>0; x--){
+//     if(x%3==0){
+//         console.log(x);
+//     }
+// }
+
+// Print the sequence
+// version 1
+// var arr= [4,2.5,1,-0.5,-2,-3.5];
+// for(x=0; x<arr.length; x++){
+//     console.log(arr[x]);
+// }
+// version 2
+// for(x=4; x>-4;x=x-1.5){
+//     console.log(x);
+// }
+
+// Sigma
+// var sum = 0;
+// for (x = 0; x <= 100; x++) {
+//     var sum = sum + x;
+// }
+// console.log(sum);
+
+//factorial
+// var product = 1;
+// for(x=1; x<=12; x++){
+//     var product = product * x;
+// }
+// console.log(product);
